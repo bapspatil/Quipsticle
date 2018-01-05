a9ea400e4d58dc7973f8e70abc140a2b13e075a1
Bapusaheb Patil <bapoo1998@gmail.com>
2018-01-06 03:31:52 +0530

    Setting up views according to starter code

--- app/src/main/java/bapspatil/quipsticle/MainActivity.java
+++ app/src/main/java/bapspatil/quipsticle/MainActivity.java
@@ -1,7 +1,9 @@
 package bapspatil.quipsticle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void tellJoke(View view) {
+        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/bapspatil/quipsticle/MainActivityFragment.java
@@ -0,0 +1,40 @@
+package bapspatil.quipsticle;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainActivityFragment extends Fragment {
+
+
+    public MainActivityFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        AdView mAdView = root.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+        return root;
+    }
+
+}
